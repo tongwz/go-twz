4e95a4390f1b333a9b29eaa065c560d1b689a4f7
tongWz <tongweizhen@foxmail.com>
2023-04-23 16:51:41 +0800

    xiugai

--- src/go.twz/xss/main.go
+++ src/go.twz/xss/main.go
@@ -1,9 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	gEngin := gin.New()
-	gEngin.Static("/index", "")
-	gEngin.LoadHTMLGlob("")
+	// gEngin.Static("/index", "./")
+	gEngin.LoadHTMLGlob("view/*")
+
+	gEngin.GET("/index", Hello)
+	gEngin.POST("/index", Hello2)
+
+	gEngin.Run(":8011")
+}
+
+func Hello(ctx *gin.Context) {
+	ctx.HTML(200, "index.html", nil)
+}
+
+func Hello2(ctx *gin.Context) {
+	info := ctx.PostForm("name")
+	ctx.Data(200, "text/html", []byte(info))
+	// ctx.String(200, info)
 }
