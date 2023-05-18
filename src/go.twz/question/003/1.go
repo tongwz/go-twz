bb20e870626c8ae3007612a5e56f3259af77d3ac
tongWz <tongweizhen@foxmail.com>
2023-05-18 17:27:51 +0800

    xiugai

---
+++ src/go.twz/question/003/1.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	str := "122344556767dfgdhfghjghkjasfs"
+	str2 := "123456789a"
+
+	myReverseStr(&str)
+
+	fmt.Printf("翻转后的值是str： %s \n", str)
+	myReverseStr(&str2)
+	fmt.Printf("翻转后的值是str2： %s \n", str2)
+}
+
+func myReverseStr(s *string) bool {
+	strSli := []int32(*s)
+
+	if len(strSli) > 5000 {
+		return false
+	}
+
+	// 10
+	length := len(strSli)
+
+	for key, _ := range strSli {
+		if key > (length / 2) {
+			break
+		}
+		strSli[key], strSli[length-key-1] = strSli[length-key-1], strSli[key]
+	}
+
+	ss := string(strSli)
+	*s = ss
+	return true
+}
