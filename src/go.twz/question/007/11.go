cbe3931193fa2a49508465285c03284f5bc290d4
tongWz <tongweizhen@foxmail.com>
2023-06-12 19:37:40 +0800

    xiugai

--- src/go.twz/question/007/11.go
+++ src/go.twz/question/007/11.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	runtime.GOMAXPROCS(1)
+	var i byte
+	go func() {
+		for i = 0; i <= 255; i++ {
+			fmt.Println(i)
+		}
+	}()
+	fmt.Println("Dropping mic")
+
+	// Yield execution to force executing other goroutines
+	start := time.Now()
+	runtime.Gosched()
+
+	runtime.GC()
+	cost := time.Since(start)
+
+	fmt.Println("Done", cost)
+}
+
+// 这里会让渡出当前goroutine的执行权，但是它并没有挂起当前的goroutine，因此她后续还是会自动恢复执行；
