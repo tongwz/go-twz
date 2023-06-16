465110925e8d63963d895d437a6dfb73fa3e2de6
童伟珍 <tongweizhen@foxmail.com>
2023-06-16 22:32:04 +0800

    xiugai

--- src/go.twz/question/008/1.go
+++ src/go.twz/question/008/1.go
@@ -5,19 +5,23 @@ import (
 )
 
 func main() {
-	defer_call()
-}
-
-func defer_call() {
-	defer func() { fmt.Println("打印前") }()
-	defer func() { fmt.Println("打印中") }()
-	defer func() { fmt.Println("打印后") }()
-
 	defer func() {
 		if err := recover(); err != nil {
 			fmt.Println(err)
 		}
 	}()
+	defer_call()
+	defer_call()
+}
+
+func defer_call() {
+
+	defer func() { fmt.Println("打印前") }()
+	defer func() { fmt.Println("打印中") }()
+	defer func() { fmt.Println("打印后") }()
 
 	panic("触发异常")
+	fmt.Println(21)
 }
+
+// 而函数的退出顺序是后进先出的。所以，最后一个 defer 函数会最先执行，而第一个 defer 函数会最后执行。这种执行方式可以确保资源的正确释放，例如文件句柄、数据库连接等资源的关闭。
