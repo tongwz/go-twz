ef9c3e07d893b00c8bc723acaff2bc48b64ac0aa
童伟珍 <tongweizhen@foxmail.com>
2023-06-20 09:45:26 +0800

    xiugai

---
+++ src/go.twz/question/008/4.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type People struct{}
+
+func (p *People) ShowA() {
+	fmt.Println("showA")
+	p.ShowB()
+}
+func (p *People) ShowB() {
+	fmt.Println("showB")
+}
+
+type Teacher struct {
+	People
+}
+
+func (t *Teacher) ShowB() {
+	fmt.Println("teacher showB")
+}
+
+func main() {
+	t := Teacher{}
+	t.ShowA()
+}
