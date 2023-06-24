396a0a4f0f8e668817c78eec372c6987b24fec34
童伟珍 <tongweizhen@foxmail.com>
2023-06-24 10:45:25 +0800

    xiugai

--- src/go.twz/question/008/4.go
+++ src/go.twz/question/008/4.go
@@ -1,26 +1,40 @@
 package main
 
-import "fmt"
-
 type People struct{}
 
 func (p *People) ShowA() {
-	fmt.Println("showA")
+	println("showA")
+	// fmt.Printf("当前对象：%#v \n", p)
 	p.ShowB()
 }
 func (p *People) ShowB() {
-	fmt.Println("showB")
+	println("showB")
 }
 
 type Teacher struct {
-	People
+	P1 People
+	People2
 }
 
 func (t *Teacher) ShowB() {
-	fmt.Println("teacher showB")
+	println("teacher showB")
 }
 
 func main() {
 	t := Teacher{}
+
+	println("获取teacher中People地址：%p", &t.People2)
+
 	t.ShowA()
+}
+
+type People2 struct{}
+
+func (p *People2) ShowA() {
+	println("show People2 A")
+	// fmt.Printf("当前对象：%#v \n", p)
+	p.ShowB()
+}
+func (p *People2) ShowB() {
+	println("show People2 B")
 }
