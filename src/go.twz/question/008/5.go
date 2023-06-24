86874bf7f7b449ea42bb7b75d6930398a4bbe194
童伟珍 <tongweizhen@foxmail.com>
2023-06-24 11:01:27 +0800

    xiugai

---
+++ src/go.twz/question/008/5.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	runtime.GOMAXPROCS(4)
+	int_chan := make(chan int, 1)
+	string_chan := make(chan string, 1)
+	int_chan <- 1
+	string_chan <- "hello"
+	select {
+	case value := <-int_chan:
+		fmt.Println(value)
+	case value := <-string_chan:
+		panic(value)
+	}
+}
