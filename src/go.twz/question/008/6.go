93a5a45fad77df36427135561e6c2b9fa0964f04
童伟珍 <tongweizhen@foxmail.com>
2023-06-25 22:44:08 +0800

    xiugai

--- src/go.twz/question/008/6.go
+++ src/go.twz/question/008/6.go
@@ -11,8 +11,10 @@ func calc(index string, a, b int) int {
 func main() {
 	a := 1
 	b := 2
-	defer calc("1", a, calc("10", a, b))
+	defer calc("1", a, calc("10", a, b)) // 10 1 2 3
+	// 1 1 3 4
 	a = 0
-	defer calc("2", a, calc("20", a, b))
+	defer calc("2", a, calc("20", a, b)) // 20 0 2 2
+	// 2 0 2 2
 	b = 1
 }
