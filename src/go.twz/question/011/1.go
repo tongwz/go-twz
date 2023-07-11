2a689db6e55e9f60d765f8894b2b8cb520c9af6d
童伟珍 <tongweizhen@foxmail.com>
2023-07-11 08:29:04 +0800

    xiugai

---
+++ src/go.twz/question/011/1.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Ban struct {
+	visitIPs map[string]time.Time
+}
+
+func NewBan() *Ban {
+	return &Ban{visitIPs: make(map[string]time.Time)}
+}
+func (o *Ban) visit(ip string) bool {
+	if _, ok := o.visitIPs[ip]; ok {
+		return true
+	}
+	o.visitIPs[ip] = time.Now()
+	return false
+}
+func main() {
+	success := 0
+	ban := NewBan()
+	for i := 0; i < 1000; i++ {
+		for j := 0; j < 100; j++ {
+			go func() {
+				ip := fmt.Sprintf("192.168.1.%d", j)
+				if !ban.visit(ip) {
+					success++
+				}
+			}()
+		}
+
+	}
+	fmt.Println("success:", success)
+}
