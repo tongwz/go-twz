68cd688dc63ac5ce900b694013fdfbbbf5ec899d
童伟珍 <tongweizhen@foxmail.com>
2023-08-13 14:06:41 +0800

    xiugia

--- src/go.twz/question/015/4.go
+++ src/go.twz/question/015/4.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"sync"
+	"time"
 )
 
 type Once struct {
@@ -9,7 +11,8 @@ type Once struct {
 	done uint32
 }
 
-func (o *Once) Do(f func()) {
+func (o *Once) Do(f func(), wait *sync.WaitGroup) {
+	defer wait.Done()
 	if o.done == 1 {
 		return
 	}
@@ -19,4 +22,30 @@ func (o *Once) Do(f func()) {
 		o.done = 1
 		f()
 	}
+}
+
+func main() {
+	// 内存可见性（memory visibility）
+	// 内存屏障（memory barrier）
+	// ww := sync.Once{}
+	wait := sync.WaitGroup{}
+	for j := 0; j < 10000; j++ {
+		wait.Add(10000)
+		d := new(Once)
+
+		f := func() {
+			fmt.Printf("我执行了   ")
+		}
+
+		for i := 0; i < 10000; i++ {
+			go d.Do(f, &wait)
+		}
+		wait.Wait()
+		fmt.Printf("结束！ \n")
+		if j%50 == 0 {
+			time.Sleep(time.Second)
+		}
+	}
+
+	time.Sleep(time.Millisecond * 1000)
 }
