e9c320734cd831089355090ad053577b84a6f4dc
tongWz <tongweizhen@foxmail.com>
2023-09-01 09:50:29 +0800

    xiugia

--- src/go.twz/question/015/7.go
+++ src/go.twz/question/015/7.go
@@ -10,14 +10,17 @@ func main() {
 	var ch chan int
 	go func() {
 		ch = make(chan int, 1)
+		fmt.Printf("我走到这里了2 %p \n", ch)
 		ch <- 1
 	}()
 	go func(ch chan int) {
+		fmt.Printf("我走到这里了 %p \n", ch)
 		time.Sleep(time.Second)
 		<-ch
+		fmt.Printf("结束")
 	}(ch)
 	c := time.Tick(1 * time.Second)
 	for range c {
-		fmt.Printf("#goroutines: %d\n", runtime.NumGoroutine())
+		fmt.Printf("#goroutines: %d \n", runtime.NumGoroutine())
 	}
 }
