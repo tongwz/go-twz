6bd593419589d6217431732e21a17b176a925743
童伟珍 <tongweizhen@foxmail.com>
2023-09-20 23:02:25 +0800

    xiugai

--- src/go.twz/question/015/7.go
+++ src/go.twz/question/015/7.go
@@ -12,13 +12,17 @@ func main() {
 		ch = make(chan int, 1)
 		fmt.Printf("我走到这里了2 %p \n", ch)
 		ch <- 1
+		fmt.Printf("结束1 \n")
 	}()
 	go func(ch chan int) {
 		fmt.Printf("我走到这里了 %p \n", ch)
 		time.Sleep(time.Second)
 		<-ch
-		fmt.Printf("结束")
+		fmt.Printf("结束2 \n")
 	}(ch)
+
+	var myMap map[string]string = nil
+	fmt.Printf("打印没有make的map 值是：%s \n", myMap["基尼太美"])
 	c := time.Tick(1 * time.Second)
 	for range c {
 		fmt.Printf("#goroutines: %d \n", runtime.NumGoroutine())
