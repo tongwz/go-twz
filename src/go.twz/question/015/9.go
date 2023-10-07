e5221e19629c895d625756434675ba1468e7a22c
童伟珍 <tongweizhen@foxmail.com>
2023-10-07 11:54:40 +0800

    xiugai

--- src/go.twz/question/015/9.go
+++ src/go.twz/question/015/9.go
@@ -9,5 +9,15 @@ func main() {
 	var m sync.Map
 	m.LoadOrStore("a", 1)
 	m.Delete("a")
-	fmt.Println(m.Len())
+	// fmt.Println(m.Len())
+	m.LoadOrStore("b", 2)
+	m.LoadOrStore("c", 3)
+	m.LoadOrStore("c", 2)
+
+	var count int
+	m.Range(func(key, value any) bool {
+		count++
+		return true
+	})
+	fmt.Println(count)
 }
