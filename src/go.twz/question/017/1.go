de5776c114a4dbdf3cf18878cf2693edc8c3ce9e
tongWz <tongweizhen@foxmail.com>
2023-11-02 11:24:15 +0800

    xiugai

--- src/go.twz/question/017/1.go
+++ src/go.twz/question/017/1.go
@@ -11,16 +11,20 @@ func main() {
 	for i := 11; i < 125; i++ {
 		sData = append(sData, i)
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
 	isFind := make(chan int)
-	go FindNeedNum(ctx, sData, 100, isFind)
+	go FindNeedNum(ctx, sData, 96, isFind)
 	for {
 		select {
+		case <-ctx.Done():
+			fmt.Println("超时了~")
+			return
 		case val := <-isFind:
 			if val >= 0 {
 				fmt.Printf("找到了数字index = %d 结束代码 \n", val)
 				cancel()
-				time.Sleep(time.Second * 3)
+				time.Sleep(time.Second * 2)
 				fmt.Println("结束")
 				return
 			}
@@ -37,24 +41,23 @@ func FindNeedNum(ctx context.Context, sData []int, num int, isFind chan int) {
 	// 分10个协程处理
 	for i := 0; i < 10; i++ {
 		go func(times int, ctx context.Context) {
-			for {
+			for j := gNum * times; j < gNum*(times+1); j++ {
 				select {
 				case <-ctx.Done():
 					fmt.Printf("程序被Done终止  \n")
 					return
 				default:
 					// 按照索引查询数据
-					for j := gNum * times; j < gNum*(times+1); j++ {
-						time.Sleep(time.Second * 1)
-						if j >= sLen {
-							return
-						}
-						fmt.Println(sData[j])
-						if sData[j] == num {
-							isFind <- j
-							return
-						}
+					time.Sleep(time.Millisecond * 500)
+					if j >= sLen {
+						return
 					}
+					fmt.Println(sData[j])
+					if sData[j] == num {
+						isFind <- j
+						return
+					}
+
 				}
 			}
 
