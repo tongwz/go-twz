07c479ccf05d49a24af2d1242d6873addbac3eb6
tongWz <tongweizhen@foxmail.com>
2024-01-15 19:26:24 +0800

    xiugai

--- src/go.twz/question/013/3.go
+++ src/go.twz/question/013/3.go
@@ -2,27 +2,49 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 	"time"
 )
 
 func main() {
+	for i := 0; i < 1; i++ {
+		go SetFunc()
+	}
+	time.Sleep(time.Second * 3)
+	numGoroutines := runtime.NumGoroutine()
+	fmt.Println("当前程序中有", numGoroutines, "个活跃的 goroutine")
+
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("Alloc = %v iB \n", m.Alloc)
+
+	runtime.GC()
+
+	runtime.ReadMemStats(&m)
+	fmt.Printf("Alloc = %v iB \n", m.Alloc)
+
+	time.Sleep(time.Second * 3)
+}
+
+func SetFunc() {
 	wg := sync.WaitGroup{}
 	c := make(chan struct{})
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 3; i++ {
 		wg.Add(1)
 		go func(num int, close <-chan struct{}) {
 			defer wg.Done()
 			<-close
-			fmt.Println(num)
+			// fmt.Println(num)
 		}(i, c)
 	}
+	numGoroutines := runtime.NumGoroutine()
+	fmt.Println("当前程序中有", numGoroutines, "个活跃的 goroutine")
 
-	if WaitTimeout(&wg, time.Second*5) {
+	if WaitTimeout(&wg, time.Second*2) {
 		close(c)
-		fmt.Println("timeout exit")
+		// fmt.Println("timeout exit")
 	}
-	time.Sleep(time.Second * 10)
 }
 
 func WaitTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
@@ -30,11 +52,16 @@ func WaitTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
 	// 要求sync.WaitGroup支持timeout功能
 	// 如果timeout到了超时时间返回true
 	// 如果WaitGroup自然结束返回false
+	// ch := make(chan bool)
 	ch := make(chan bool, 1)
 
-	go time.AfterFunc(timeout, func() {
-		ch <- true
-	})
+	timer := time.NewTimer(timeout)
+	go func() {
+		select {
+		case <-timer.C:
+			ch <- true
+		}
+	}()
 
 	go func() {
 		wg.Wait()
